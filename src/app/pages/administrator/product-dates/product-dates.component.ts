eb6b17180c927f53710264e9a97fd3ae958159b3
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2023-10-17 17:41:27 -0400

    creando el campo para el producto

---
+++ src/app/pages/administrator/product-dates/product-dates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-dates',
+  templateUrl: './product-dates.component.html',
+  styleUrls: ['./product-dates.component.css']
+})
+export class ProductDatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
