4a30c91f6a515be4c71a2d8474906f970d14a754
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2023-10-18 20:14:36 -0400

    Modificando datos

--- src/app/pages/administrator/inventory/inventory.component.ts
+++ src/app/pages/administrator/inventory/inventory.component.ts
@@ -140,9 +140,8 @@ export class InventoryComponent implements OnInit {
         {
           targets: 6,
           render: function (data: any, type: any, row: any) {
-              return '<a href="javascript:void(0);" class="btn btn-sm btn-light btn-active-light-primary" data-watch-id="' + row.id + '" data-bs-toggle="modal" data-bs-target="#kt_modal_update_product_1">Ver' +
-                  '</a>'+
-                  '<a  href="javascript:void(0);" class="btn btn-sm btn-light btn-active-light-primary" data-product-id="' + row.id + '">edit</a>';
+              return '<a href="javascript:void(0);" class="btn bg-body" data-watch-id="' + row.id + '" data-bs-toggle="modal" data-bs-target="#kt_modal_update_product_1">    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="blue" class="bi bi-eye-fill" viewBox="0 0 16 16"><path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/><path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/></svg></a>'+
+                  '<a  href="javascript:void(0);" class="btn bg-body" data-product-id="' + row.id + '">    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pencil-fill" viewBox="0 0 16 16"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg></a>';
 
           }
       }
